56e7fff1c407d1245d0ff3a2922314a3aa69cd88
KyawThetHtwe5502 <kyawthethtwe5502@gmail.com>
2025-01-22 21:40:26 +0630

    setparams

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -9,7 +9,7 @@ import {
     HiPlus
 } from "react-icons/hi2";
 import {
-    HiSearch 
+    HiSearch
 } from "react-icons/hi";
 import { Button } from "@/components/ui/button"
 
@@ -24,45 +24,52 @@ import { Input } from "./ui/input";
 
 export type Product = {
     id: number;
-    product_name : string;
+    product_name: string;
     created_at: string;
     price: number;
 }
 
 const ProductList = () => {
-    const [params,setParams] = useSearchParams();
-    console.log(params.get('a'));
-    const [token] = useCookie('my_token')
-    const [fetchUrl,setFetchUrl] = useState('https://voucher-app-api.ygnsh.com/api/v1/products')
-const fetcher = (url: string) => fetch(url,{
-    headers: {
-        Authorization : `Bearer ${token}`
+    const [params, setParams] = useSearchParams();
+    const location = useLocation()
+    console.log(location)
+    const currentParams = params.entries()
+    console.log(currentParams)
+    for (let x of currentParams){
+        console.log(x)
     }
-}).then((res) => res.json());
-    const { data,  isLoading } = useSWR<Product[]>(fetchUrl, fetcher);
+    const [token] = useCookie('my_token')
+    const [fetchUrl, setFetchUrl] = useState('https://voucher-app-api.ygnsh.com/api/v1/products')
+    const fetcher = (url: string) => fetch(url, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }).then((res) => res.json());
+    const { data, isLoading } = useSWR<Product[]>(fetchUrl, fetcher);
     console.log(data);
-    const updateFetchUrl = (url:string) => {
+    const updateFetchUrl = (url: string) => {
+        console.log(url)
         const currentUrl = new URL(url);
         console.log(currentUrl);
-        setFetchUrl(url)
+        // setFetchUrl(url)
     }
-    const handleSearchInput = (e:any) => {
+    const handleSearchInput = (e: any) => {
         setFetchUrl(`https://voucher-app-api.ygnsh.com/api/v1/products?q=${e.target.value}`)
     }
 
-    
+
     return (
         <div>
             <div className="flex justify-between items-center mb-6">
                 <div className="relative">
-                <Input type="text" onChange={handleSearchInput} />
-                <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                    <HiSearch className="w-4 h-4 text-stone-500 dark:text-stone-400" />
-                </div>
+                    <Input type="text" onChange={handleSearchInput} />
+                    <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                        <HiSearch className="w-4 h-4 text-stone-500 dark:text-stone-400" />
+                    </div>
                 </div>
                 <Link to="/dashboard/product/create">
-                <Button className="bg-blue-600 ">Add new Product  <HiPlus />
-                </Button>
+                    <Button className="bg-blue-600 ">Add new Product  <HiPlus />
+                    </Button>
                 </Link>
             </div>
             <Table>
@@ -77,13 +84,13 @@ const fetcher = (url: string) => fetch(url,{
                 </TableHeader>
                 <TableBody>
 
-                    {isLoading ?  <div>loading</div> : data?.length === 0 ? (<div>dfdf</div>) : (
-                        data?.data?.map((product) => <ProductRow key={product.id} product={product} /> )
+                    {isLoading ? <div>loading</div> : data?.length === 0 ? (<div>dfdf</div>) : (
+                        data?.data?.map((product) => <ProductRow key={product.id} product={product} />)
                     )}
 
                 </TableBody>
             </Table>
-            {!isLoading && <Pagination links={data?.links} meta={data?.meta} updateFetchUrl={updateFetchUrl} />  }
+            {!isLoading && <Pagination links={data?.links} meta={data?.meta} updateFetchUrl={updateFetchUrl} />}
         </div>
     )
 }
